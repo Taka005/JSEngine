beccf22765d65e06461698023f0222ff57ff90fd
Taka005 <01taka005@gmail.com>
2023-11-09 13:04:43 +0900

    fix

--- Engine.js
+++ Engine.js
@@ -31,7 +31,7 @@ class Engine{
       this.updatePosition(entity);
     });
 
-    for(let i = 0;i < 100;i++){
+    for(let i = 0;i < 10;i++){
       Object.values(this.entities).forEach(entity=>{
         Object.values(this.entities).forEach(target=>{
           if(entity.name === target.name) return;
