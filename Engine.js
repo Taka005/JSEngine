ef005472ba02048a2fccbcaee8499c552c6f0bc1
Taka005 <01taka005@gmail.com>
2023-11-09 21:21:47 +0900

    fix

--- Engine.js
+++ Engine.js
@@ -5,7 +5,7 @@ class Engine{
    * @param {Number} option.fps 描画FPS
    * @param {Number} option.gravity 重力加速度
    */
-  constructor(canvas,{fps = 60, gravity = 100, friction = 0.01} = {}){
+  constructor(canvas,{fps = 60, gravity = 100, friction = 0.001} = {}){
     this.canvas = canvas;
     this.ctx = this.canvas.getContext("2d");
 
