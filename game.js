d90a841f20194a42b421584e0485ed9488e452cd
Taka005 <01taka005@gmail.com>
2023-11-10 13:07:47 +0900

    fix

--- game.js
+++ game.js
@@ -4,9 +4,18 @@ const engine = new Engine(canvas);
 engine.start();
 
 engine.spawn({
+  name: "G5",
+  posX: 250,
+  posY: 500,
+  size: 50,
+  mass: 0,
+  stiff: 0.5
+});
+
+engine.spawn({
   name: "G1",
   posX: 350,
-  posY: 400,
+  posY: 500,
   size: 50,
   mass: 0,
   stiff: 0.5
@@ -15,7 +24,7 @@ engine.spawn({
 engine.spawn({
   name: "G2",
   posX: 450,
-  posY: 400,
+  posY: 500,
   size: 50,
   mass: 0,
   stiff: 0.5
@@ -24,11 +33,21 @@ engine.spawn({
 engine.spawn({
   name: "G3",
   posX: 550,
-  posY: 400,
+  posY: 500,
   size: 50,
   mass: 0,
   stiff: 0.5
 });
+
+engine.spawn({
+  name: "G4",
+  posX: 650,
+  posY: 500,
+  size: 50,
+  mass: 0,
+  stiff: 0.5
+});
+
 
 document.addEventListener("keydown",(event)=>{
   event.preventDefault();
