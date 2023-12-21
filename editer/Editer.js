297822b18b1ddd300713291b3a6417c5d6612c16
Taka005 <01taka005@gmail.com>
2023-12-21 17:16:01 +0900

    add

---
+++ editer/Editer.js
@@ -0,0 +1,198 @@
+class Editer{
+  /**
+   * @param {Element} canvas 適用するCanvasエレメント
+   * @param {Object} option オプション
+   * @param {Number} option.fps 描画FPS
+   * @param {Number} option.gravity 重力加速度
+   * @param {Number} option.friction 摩擦係数
+   */
+  constructor(canvas,{fps = 60, gravity = 500, friction = 0.001, restraint = 3} = {}){
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+
+    this.fps = fps;
+    this.gravity = gravity;
+    this.friction = friction;
+    this.restraint = restraint;
+
+    this.entities = {};
+    this.grounds = {};
+  }
+
+  createId(length){
+    const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let id = "";
+    for(let i = 0;i < length;i++){
+      id += str.charAt(Math.floor(Math.random()*str.length));
+    }
+
+    return id;
+  }
+
+  start(){
+    setInterval(()=>{
+      this.draw();
+    },1000/this.fps);
+  }
+
+  draw(){
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+
+    this.drawSquares();
+
+    Object.values(this.grounds).forEach(ground=>{
+      ground.draw(this.ctx);
+    });
+
+    Object.values(this.entities).forEach(entity=>{
+      entity.draw(this.ctx);
+    });
+  }
+
+  /**
+   * @param {String} type エンティティー名
+   * @param {Object} data エンティティーデータ(Entityクラスを参照してください)
+   * @returns {Entity} 生成されたエンティティークラス
+   */
+  spawn(type,data){
+    data.name = data.name || this.createId(8);
+
+    if(type === "entity"){
+      this.entities[data.name] = new Entity(data);
+
+      return this.entities[data.name];
+    }else if(type === "ground"){
+      this.grounds[data.name] = new Ground(data);
+
+      return this.grounds[data.name];
+    }
+  }
+
+  drawSquares(){
+    this.ctx.beginPath();
+
+    for(let x = 0;x < this.canvas.width;x += 25){
+      this.ctx.moveTo(x,0);
+      this.ctx.lineTo(x,this.canvas.height);
+    }
+
+    for(let y = 0;y < this.canvas.height;y += 25){
+      this.ctx.moveTo(0,y);
+      this.ctx.lineTo(this.canvas.width,y);
+    }
+
+    this.ctx.strokeStyle = "black";
+    this.ctx.lineWidth = 0.1;
+    this.ctx.stroke();
+  }
+
+  /**
+   * @returns {String} エクスポートデータ
+   */
+  export(){
+    return JSON.stringify({
+      fps: this.fps,
+      gravity: this.gravity,
+      friction: this.friction,
+      restraint: this.restraint,
+      entity: Object.values(this.entities),
+      ground: Object.values(this.grounds)
+    });
+  }
+}
+
+class Entity{
+  /**
+   * @param {Object} data エンティティーデータ
+   * @param {String} data.name エンティティー名
+   * @param {Number} data.posX X位置
+   * @param {Number} data.posY Y位置
+   * @param {Number} data.size 大きさ
+   * @param {Number} data.mass 質量
+   * @param {Number} data.stiff 剛性(0以上1以下)
+   * @param {Number} data.speedX X速度
+   * @param {Number} data.speedY Y速度
+   * @param {String} data.image 表示画像
+   */
+  constructor({name, posX, posY, size, mass, stiff, speedX = 0, speedY = 0, image = null}){
+    if(size < 0) throw new Error("サイズは0以上にしてください");
+    if(mass < 0) throw new Error("質量は0以上にしてください");
+    if(stiff < 0 || stiff > 1) throw new Error("剛性は0以上1以下にしてください");
+
+    if(image){
+      this.img = new Image();
+      this.img.src = image;
+    }
+
+    this.name = name;
+
+    this.posX = posX;
+    this.posY = posY;
+    this.prePosX = posX;
+    this.prePosY = posY;
+
+    this.speedX = speedX;
+    this.speedY = speedY;
+
+    this.size = size;
+    this.mass = mass;
+    this.stiff = stiff;
+  }
+
+  /**
+   * @param {CanvasRenderingContext2D} ctx Canvas
+   */
+  draw(ctx){
+    if(this.img){
+      ctx.drawImage(
+        this.img,
+        this.posX - this.img.width/2,
+        this.posY - this.img.height/2
+      );
+    }else{
+      ctx.beginPath();
+      ctx.arc(this.posX,this.posY,this.size,0,2*Math.PI);
+      ctx.strokeStyle = "red";
+      ctx.fillStyle = "red";
+      ctx.lineWidth = 1;
+      ctx.fill();
+      ctx.stroke();
+    }
+  }
+}
+
+class Ground{
+  /**
+   * @param {Object} data エンティティーデータ
+   * @param {String} data.name エンティティー名
+   * @param {Number} data.startX X開始位置
+   * @param {Number} data.startY Y開始位置
+   * @param {Number} data.endX X終了位置
+   * @param {Number} data.endY Y終了位置
+   * @param {Number} data.size 大きさ
+   */
+  constructor({name, startX, startY, endX, endY, size}){
+    if(size < 0) throw new Error("サイズは0以上にしてください");
+
+    this.name = name;
+
+    this.startX = startX;
+    this.startY = startY;
+    this.endX = endX;
+    this.endY = endY;
+
+    this.size = size;
+  }
+
+  /**
+   * @param {CanvasRenderingContext2D} ctx Canvas
+   */
+  draw(ctx){
+    ctx.beginPath();
+    ctx.moveTo(this.startX,this.startY);
+    ctx.lineTo(this.endX,this.endY);
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = this.size;
+    ctx.stroke();
+  }
+}
---
+++ editer/editer.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementById("game");
+const editer = new Editer(canvas);
+
+editer.start();
