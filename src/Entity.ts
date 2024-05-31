f08fa4e27d4a586543225d8c28fafefe861536f6
Taka005 <01taka005@gmail.com>
2024-05-31 03:01:07 +0000

    add

--- src/Entity.ts
+++ src/Entity.ts
@@ -51,25 +51,44 @@ class Entity{
     this.targets = targets;
   }
 
+  /**
+   * 質量の逆数を返します
+   * @returns {number} 逆数の質量
+   */
   get invMass(): number{
     if(this.mass === 0) return 0;
 
     return 1/(this.mass);
   }
 
+  /**
+   * 位置を保存します
+   */
   savePosition(): void{
     this.prePosX = this.posX;
     this.prePosY = this.posY;
   }
 
+  /**
+   * 接続対象を追加します
+   * @param target 接続するEntity
+   */
   addTarget(target: Target){
     this.targets.push(target);
   }
 
+  /**
+   * 接続対象を削除します
+   * @param targetId 削除するEntity名
+   */
   removeTarget(targetId: string){
     this.targets = this.targets.filter(target=>target.name !== targetId);
   }
 
+  /**
+   * JSONに変換します
+   * @return {EntityOption}
+   */
   toJSON(): EntityOption{
     return {
       name: this.name,
