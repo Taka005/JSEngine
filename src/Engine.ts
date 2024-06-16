bf8129af38105c9ead1289ec990636435f2fc2f8
Taka005 <01taka005@gmail.com>
2024-06-16 11:46:03 +0900

    test

--- src/Engine.ts
+++ src/Engine.ts
@@ -461,14 +461,14 @@ class Engine extends Process{
       this.ctx.moveTo(posX,-this.posY);
       this.ctx.lineTo(posX,this.canvas.height - this.posY);
 
-      this.ctx.fillText(Math.round(posX),posX,-this.posY + 10);
+      this.ctx.fillText(Math.round(posX),posX,-this.posY - 10);
     }
 
     for(let posY: number = -startY;posY < this.canvas.height - this.posY;posY += 25){
       this.ctx.moveTo(-this.posX,posY);
       this.ctx.lineTo(this.canvas.width - this.posX,posY);
 
-      this.ctx.fillText(Math.round(posY),-this.posX + 10,posY);
+      this.ctx.fillText(Math.round(posY),-this.posX - 10,posY);
     }
 
     this.ctx.strokeStyle = "black";
