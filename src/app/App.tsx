e4d95136cd1b28e4e443e73fb3c2a8cdebfaeaaa
Taka005 <01taka005@gmail.com>
2024-07-12 12:54:57 +0000

    add

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {};
+interface State {};
+
+class App extends React.Component<Props,State>{
+  render(): JSX.Element{
+    return (
+      <div>
+        テスト
+      </div>
+    )
+  }
+}
+
+export { App };
