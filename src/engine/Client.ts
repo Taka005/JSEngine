e4ad22f61210d26df3f33723228e7701030f8da5
Taka005 <01taka005@gmail.com>
2024-07-15 10:03:53 +0900

    change

--- src/engine/Client.ts
+++ src/engine/Client.ts
@@ -14,9 +14,16 @@ type ClientOption = {
  */
 class Client{
   constructor({ canvas }: ClientOption = {}){
-
+    if(canvas){
+      this.setCanvas(canvas);
+    }
   }
 
+  setCanvas(canvas: HTMLCanvasElement): void{
+    this.canvas = canvas;
+
+    this.engine = new Engine(this.canvas);
+  }
 }
 
 export { Client };
