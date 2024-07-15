31f6ecde0a9c386df030ec377443c931f182753d
Taka005 <01taka005@gmail.com>
2024-07-15 11:01:29 +0000

    change

--- src/app/components/Tooltip.tsx
+++ src/app/components/Tooltip.tsx
@@ -14,16 +14,21 @@ class Tooltip extends React.Component<TooltipProps>{
     super(props);
 
     this.textRef = React.createRef();
+
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
-  onMouseEnter(): void{
-    if(!this.textRef.current) return;
+  handleMouseEnter(): void{
+    if(!this.textRef.current) return alert("内部エラー: ツールチップの要素が取得できません");
+
     this.textRef.current.style.opacity = "1";
     this.textRef.current.style.visibility = "visible";
   }
 
-  onMouseLeave(): void{
-    if(!this.textRef.current) return;
+  handleMouseLeave(): void{
+    if(!this.textRef.current) return alert("内部エラー: ツールチップの要素が取得できません");
+
     this.textRef.current.style.opacity = "0";
     this.textRef.current.style.visibility = "hidden";
   }
@@ -34,12 +39,15 @@ class Tooltip extends React.Component<TooltipProps>{
         <div
           className="flex before:block absolute before:absolute top-full before:-top-1 left-1/2 before:left-1/2 invisible z-10 before:z-0 items-center py-[2px] px-2 mx-auto mt-2 before:w-2 before:h-2 text-xs text-white whitespace-nowrap before:bg-black bg-black rounded transition-all duration-150 transform before:transform before:rotate-45 -translate-x-1/2 before:-translate-x-1/2"
           ref={this.textRef}
-          onMouseEnter={this.onMouseEnter}
-          onMouseLeave={this.onMouseLeave}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
         >
           {this.props.description}
         </div>
-        <div onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+        <div 
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+        >
           {this.props.children}
         </div>
       </div>
