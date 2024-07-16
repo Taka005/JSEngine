5748207e384e1e4722cb6f18327d4f692f04c61f
Taka005 <01taka005@gmail.com>
2024-07-16 04:41:45 +0000

    fix

--- src/app/components/Tooltip.tsx
+++ src/app/components/Tooltip.tsx
@@ -37,7 +37,7 @@ class Tooltip extends React.Component<TooltipProps>{
     return (
       <div className="flex relative items-center">
         <div
-          className="flex before:block absolute before:absolute top-full before:-top-1 left-1/2 before:left-1/2 invisible z-10 before:z-0 items-center py-[2px] px-2 mx-auto mt-2 before:w-2 before:h-2 text-xs text-white whitespace-nowrap before:bg-black bg-black rounded transition-all duration-150 transform before:transform before:rotate-45 -translate-x-1/2 before:-translate-x-1/2"
+          className="flex whitespace-nowrap rounded bg-black px-2 py-1 text-white absolute -top-12 left-1/2 -translate-x-1/2 before:content-[''] before:absolute before:-translate-x-1/2 before:left-1/2 before:top-full before:border-4 before:border-transparent before:border-t-black opacity-0 group-hover:opacity-100 transition pointer-events-none"
           ref={this.textRef}
           onMouseEnter={this.handleMouseEnter}
           onMouseLeave={this.handleMouseLeave}
