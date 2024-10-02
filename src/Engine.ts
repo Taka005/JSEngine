56ae6e5de5cb84c269bad8f986a4a4b517736f8e
Taka005 <01taka005@gmail.com>
2024-10-02 17:52:31 +0900

    fix:調整

--- src/Engine.ts
+++ src/Engine.ts
@@ -357,7 +357,7 @@ class Engine extends Process{
 
     if(this.isTrack){
       this.ctx.globalAlpha = 0.5;
-      this.ctx.globalCompositeOperation = "source-out";
+      this.ctx.globalCompositeOperation = "destination-out";
 
       Object.values(this.tracks).forEach(track=>{
         track.draw(this.ctx);
@@ -384,9 +384,9 @@ class Engine extends Process{
       this.ctx.textBaseline = "middle";
       this.ctx.fillText(`${this.correntPps}PPS`,38,15);
       this.ctx.fillText(`${this.correntFps}FPS`,110,15);
-      this.ctx.fillText(`${Object.values(this.objects).length + Object.values(this.grounds).length}Objects`,192,15);
+      this.ctx.fillText(`${Object.values(this.objects).length + Object.values(this.grounds).length}Objects`,194,15);
       this.ctx.fillText(`${this.entities.length}Entities`,290,15);
-      this.ctx.fillText(`${this.tracks.length}Tracks`,340,15);
+      this.ctx.fillText(`${this.tracks.length}Tracks`,360,15);
     }
 
     this.dispatchEvent(new CustomEvent(Event.DrawUpdate));
