f9a6cb62ac44903c6d2de29cf4d8fa3f89c12c53
Taka005 <01taka005@gmail.com>
2024-10-22 12:34:32 +0900

    fix:miss

--- src/Effects/Attractor.ts
+++ src/Effects/Attractor.ts
@@ -84,10 +84,10 @@ class Attractor{
     const vecX: number = entity.posX - this.posX;
     const vecY: number = entity.posY - this.posY;
 
-    if(
-      Math.abs(vecX) >= entity.size + this.size||
-      Math.abs(vecY) >= entity.size + this.size
-    ) return;
+    //if(
+      //Math.abs(vecX) >= entity.size + this.size||
+      //Math.abs(vecY) >= entity.size + this.size
+    //) return;
 
     const distance: number = Math.sqrt(vecX**2 + vecY**2);
     if(distance === 0) return;
