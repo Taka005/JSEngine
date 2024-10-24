3db243c9b356693b83b008d68d5815b09055d9ae
Taka005 <01taka005@gmail.com>
2024-10-24 15:57:51 +0900

    style:調整

--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,7 @@ function createId(length: number): string{
   const str: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let id: string = "";
 
-  for(let i = 0;i < length;i++){
+  for(let i: number = 0;i < length;i++){
     id += str.charAt(Math.floor(Math.random()*str.length));
   }
 
